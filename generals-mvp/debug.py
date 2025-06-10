55d3593d3e1b2ed631075bfdf90f4cda684c60b9
wjj1234321 <wujiaju4@gmail.com>
2025-06-10 22:47:59 +0800

    架构更新

--- generals-mvp/debug.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-import sys
-import os
-
-if __name__ == "__main__":
-    # 切换到 generals-mvp 目录
-    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    port = 8000
-    uvicorn.run("generals-mvp.server.app:app", host="127.0.0.1", port=port, reload=True)
